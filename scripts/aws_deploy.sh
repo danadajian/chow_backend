67c3a46cfda89567bcbf8b58cd5b1289bb3076c0
Dan Adajian <danadajian@gmail.com>
2020-06-28 14:55:45 -0500

    * more steps

--- scripts/aws_deploy.sh
+++ scripts/aws_deploy.sh
@@ -10,19 +10,10 @@ else
 fi
 
 TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
-FILE_NAME="chow-$TIMESTAMP.zip"
+export FILE_NAME="chow-$TIMESTAMP.zip"
 
 zip -r -qq "$FILE_NAME" src
 echo "### Zipped $FILE_NAME successfully."
 
 aws s3 rm "s3://${BUCKET_NAME}" --recursive --exclude "*" --include "*.zip"
 aws s3 cp "${FILE_NAME}" "s3://${BUCKET_NAME}/"
-
-echo "### Initiating SAM Deploy..."
-
-STACK_NAME="chow-stack"
-
-sam --version
-sam deploy --template-file ./template.yaml --stack-name "${STACK_NAME}" --capabilities CAPABILITY_IAM \
-  --parameter-overrides BucketName="${BUCKET_NAME}" CodeKey="${FILE_NAME}" AwsKey="${AWS_ACCESS_KEY_ID}" \
-   AwsSecret="${AWS_SECRET_ACCESS_KEY}" --no-fail-on-empty-changeset
